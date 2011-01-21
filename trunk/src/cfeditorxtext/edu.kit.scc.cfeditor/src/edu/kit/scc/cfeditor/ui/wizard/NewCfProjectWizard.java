ad153bb2e3065624cd250f8effe2e40070363b82
anbender.89 <anbender.89@eb57ff6b-286c-34f3-5897-267105310568>
2011-01-21 08:50:03 +0000

    added main project files

---
+++ trunk/src/cfeditorxtext/edu.kit.scc.cfeditor/src/edu/kit/scc/cfeditor/ui/wizard/NewCfProjectWizard.java
@@ -0,0 +1,77 @@
+package edu.kit.scc.cfeditor.ui.wizard;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.IWizard;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.wizards.IWizardDescriptor;
+
+enum CMEngine {
+	CFENGINE, PUPPET //, DUMMY
+}
+
+public class NewCfProjectWizard extends Wizard implements INewWizard {
+	private CfeditorSelectExtWizardPage extensionSelectPage;
+	private CMEngine selectedEngine = null;
+
+	public NewCfProjectWizard() {
+		setWindowTitle("New Cfeditor Project");
+	}
+
+	public void addPages() {
+		extensionSelectPage = new CfeditorSelectExtWizardPage("Cfeditor Project Wizard");
+		addPage(extensionSelectPage);
+	}
+
+	@Override
+	public boolean performFinish() {
+
+		switch (selectedEngine) {
+		case CFENGINE:
+			IWizardDescriptor descriptor = PlatformUI.getWorkbench().getNewWizardRegistry()
+					.findWizard("edu.kit.scc.cfeditor.cfengine.ui.wizard.CfengineEditorNewProjectWizard");
+
+			IWizard wizard;
+			try {
+				wizard = descriptor.createWizard();
+				WizardDialog wd = new WizardDialog(this.getShell(), wizard);
+				wd.setTitle(wizard.getWindowTitle());
+				wd.open();
+			} catch (CoreException e) {
+				return false;
+			}
+			return true;
+		case PUPPET:
+			break;
+//		case DUMMY:
+//			break;
+		}
+
+		return false;
+	}
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+	}
+
+	@Override
+	public boolean canFinish() {
+		if (null != selectedEngine) {
+			return true;
+		}
+		return false;
+	}
+
+	public void setSelectedEngine(CMEngine selectedEngine) {
+		this.selectedEngine = selectedEngine;
+	}
+
+	public CMEngine getSelectedEngine() {
+		return selectedEngine;
+	}
+
+}
